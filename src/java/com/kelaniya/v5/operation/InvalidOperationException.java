97d44cf4746a2b07f6ca4818d0d3723699869cda
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2021-09-19 22:31:47 +0530

    Add InvalidOperationException.java

---
+++ src/java/com/kelaniya/v5/operation/InvalidOperationException.java
@@ -0,0 +1,9 @@
+package com.kelaniya.v5.operation;
+
+public class InvalidOperationException extends Throwable {
+
+    public InvalidOperationException(String message) {
+
+        super(message);
+    }
+}
