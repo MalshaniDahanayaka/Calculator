2b5a6500e2dea9a0f8b4b17f48c666a78753a46e
MalshaniDahanayaka <mekaladahanayaka80@gmail.com>
2021-09-19 22:29:47 +0530

    Add exception to the Divoperation file for v5 unit testing

--- src/java/com/kelaniya/v5/operation/DivOperation.java
+++ src/java/com/kelaniya/v5/operation/DivOperation.java
@@ -1,7 +1,15 @@
 package com.kelaniya.v5.operation;
 
+import java.awt.dnd.InvalidDnDOperationException;
+
 public class DivOperation implements Operation {
-    public Double execute(Double[] numbers) {
+
+    public Double execute(Double[] numbers) throws InvalidOperationException {
+
+        if(numbers[1]==0){
+            throw new InvalidOperationException("Do not divide by zero");
+        }
+
         return numbers[0] / numbers[1];
     }
 }
